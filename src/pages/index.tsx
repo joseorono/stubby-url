2b6112b622eb9dca1326f9c18a9d7bd9b2c752b5
José Miguel Oroño <joseomaker@gmail.com>
2024-02-13 02:52:57 -0500

    Reduce home page hero height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
   return (
       <>
       <PageLayout>
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full py-12 md:py-24">
           <div className="container flex flex-col items-center px-4 md:px-6 space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
@@ -34,7 +34,7 @@ export default function HomePage() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 border-t border-gray-200 dark:border-gray-800">
+        <section className="w-full py-12 md:py-20 border-t border-gray-200 dark:border-gray-800">
           <div className="container max-w-[768px] flex flex-col gap-4 px-4 md:px-6 text-center">
             <div className="space-y-4 ">
               <h2 className="text-3xl mb-8 font-bold tracking-tighter sm:text-4xl md:text-5xl">Why I Built This</h2>
