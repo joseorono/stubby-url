8eb80251c4b011515825402fd8074bd318390bd9
José Miguel Oroño <joseomaker@gmail.com>
2024-02-13 23:23:25 -0500

    Create public utilities for URL formation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,8 +2,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { getBaseUrl } from "~/utils/api"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function linkFromId(id: string): string {
+  // This is subject to change if the routes are refactored.
+  return getBaseUrl() + "/" + id
 }
 
 // Barrel Exports
