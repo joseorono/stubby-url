7d14698eb6a6cf40df9bda478c5ee05db9263de2
José Miguel Oroño <joseomaker@gmail.com>
2024-02-17 20:28:30 -0500

    Install Toast library & style them globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { Toaster } from "react-hot-toast";
+
 import "~/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -11,9 +13,62 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+
+    <Toaster
+      
+      toastOptions={{
+
+        position: "bottom-center",
+
+        // Aria
+        ariaProps: {
+          role: 'status',
+          'aria-live': 'polite',
+        },
+
+        style: {  
+          background: 'hsl(var(--bg))',
+          color: 'hsl(var(--foreground))',
+          //fontWeight: '500',
+          //fontSize: '0.875rem',
+        },
+
+        iconTheme: {
+          primary: 'hsl(var(--primary))',
+          //secondary: 'hsl(var(--secondary))',
+          secondary: 'white',
+
+        },
+
+        /*
+        // Styling 
+        className: '',
+
+        success: {
+          style: {
+            background: '#030e18',
+            color: "#fff",
+            fontWeight: "500",
+          },
+        },
+        error: {
+          style: {
+            background: '#d32f2f',
+            color: "#fff",
+            fontWeight: "500",
+          },
+        },
+        */
+      }}
+      />
+    </>
+    
+    
+
   );
 };
 
